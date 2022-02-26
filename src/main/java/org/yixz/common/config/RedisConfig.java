993f7bee426e880b7da22096a8632fc7b4de30d2
yixiuzheng <yixiuzheng11@163.com>
2022-02-26 15:46:02 +0800

    redis配置，解决localDate类型反序列化报错的问题

--- src/main/java/org/yixz/common/config/RedisConfig.java
+++ src/main/java/org/yixz/common/config/RedisConfig.java
@@ -1,5 +1,11 @@
 package org.yixz.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,8 +23,19 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        //日期格式
+        ObjectMapper om = new ObjectMapper();
+        //将接收到参数转为对象时，参数中的某个属性在类中不存在时，不会报错
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //解决无法反序列化LocalDateTime的问题
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.registerModule(new JavaTimeModule());
+        //此项必须配置，否则反序列化时会报java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to XXX
+        //enableDefaultTyping已经过期，改成最新的activateDefaultTyping方法
+        //om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        GenericJackson2JsonRedisSerializer  jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(om);
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
